f7dc4678f7dfd582dfdbd82b7e121f84dc958feb
JuliaThom <juliathom89@web.de>
2024-04-04 00:44:19 +0200

    Adaption ui - new Bib pg8000

--- ui.py
+++ ui.py
@@ -2,46 +2,60 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-import requests
+import pg8000
 
-
-class WeatherUI(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.base_url = 'http://api.openweathermap.org/data/2.5/weather'
-        self.api_key = 'YOUR_API_KEY'
-
+class WeatherApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical', spacing=10)
+        self.layout = BoxLayout(orientation='vertical')
 
-        self.city_input = TextInput(hint_text='Enter city name')
-        layout.add_widget(self.city_input)
+        self.label_city = Label(text='Stadt: ')
+        self.layout.add_widget(self.label_city)
 
-        self.weather_label = Label(text='', size_hint_y=0.8)
-        layout.add_widget(self.weather_label)
+        self.label_temp = Label(text='Temperatur: ')
+        self.layout.add_widget(self.label_temp)
 
-        refresh_button = Button(text='Refresh', size_hint_y=0.2)
-        refresh_button.bind(on_press=self.fetch_weather_data)
-        layout.add_widget(refresh_button)
+        self.label_wind = Label(text='Windgeschwindigkeit: ')
+        self.layout.add_widget(self.label_wind)
 
-        return layout
+        self.label_max_temp = Label(text='Maximale Temperatur: ')
+        self.layout.add_widget(self.label_max_temp)
 
-    def fetch_weather_data(self, instance):
-        city = self.city_input.text.strip()
-        if city:
-            url = f'{self.base_url}?q={city}&appid={self.api_key}'
-            response = requests.get(url)
-            data = response.json()
-            if 'main' in data and 'weather' in data:
-                weather_desc = data['weather'][0]['description']
-                temperature = data['main']['temp']
-                self.weather_label.text = f'Weather: {weather_desc}, Temperature: {temperature}K'
-            else:
-                self.weather_label.text = 'Error: No data found.'
+        self.label_min_temp = Label(text='Minimale Temperatur: ')
+        self.layout.add_widget(self.label_min_temp)
+
+        refresh_button = Button(text='Daten aktualisieren')
+        refresh_button.bind(on_press=self.refresh_data)
+        self.layout.add_widget(refresh_button)
+
+        self.fetch_weather_data()
+
+        return self.layout
+
+    def fetch_weather_data(self):
+        conn = pg8000.connect(
+            user='postgres',
+            password='Monster',
+            host='localhost',
+            port=5432
+        )
+        cur = conn.cursor()
+        cur.execute('SELECT city, temperature, wind_speed, temp_max, temp_min FROM weather_data ORDER BY timestamp DESC LIMIT 1')
+        data = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        if data:
+            city, temperature, wind_speed, temp_max, temp_min = data
+            self.label_city.text = f'Stadt: {city}'
+            self.label_temp.text = f'Temperatur: {temperature} °C'
+            self.label_wind.text = f'Windgeschwindigkeit: {wind_speed} m/s'
+            self.label_max_temp.text = f'Maximale Temperatur: {temp_max} °C'
+            self.label_min_temp.text = f'Minimale Temperatur: {temp_min} °C'
         else:
-            self.weather_label.text = 'Please enter a city name.'
+            print('Keine Daten gefunden.')
 
+    def refresh_data(self, instance):
+        self.fetch_weather_data()
 
-if __name__ == '__main__':
-    WeatherUI().run()
+if __name__ == "__main__":
+    WeatherApp().run()
