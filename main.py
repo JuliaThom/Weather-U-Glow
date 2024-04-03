c56e73b4bcf6fb97b263474e54fc46f18ce0f12e
JuliaThom <juliathom89@web.de>
2024-04-04 00:23:23 +0200

    Adaption main - new Bib pg8000

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+import pg8000
 import requests
 from datetime import datetime
-import psycopg2
-
 
 
 def fetch_weather_data():
-    url = f'http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=7b256a5921981a44c938b40908d30411'
+    # Daten aus der API abrufen
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=7b256a5921981a44c938b40908d30411'
     response = requests.get(url)
     data = response.json()
 
+    # Daten aus der API parsen
     if 'main' in data and 'wind' in data:
         city = data['name']
         temperature = data['main']['temp']
@@ -16,28 +17,39 @@ def fetch_weather_data():
         temp_max = data['main']['temp_max']
         temp_min = data['main']['temp_min']
         timestamp = datetime.now()
+
+        # Daten in die Datenbank speichern
         save_to_database(timestamp, city, temperature, wind_speed, temp_max, temp_min)
     else:
         print("Fehler: Keine Daten gefunden.")
 
 
 def save_to_database(timestamp, city, temperature, wind_speed, temp_max, temp_min):
-    conn = psycopg2.connect(
-        dbname='weather',
-        user='postgres',
-        password='Monster',
-        host='localhost',
-        port='5432'
-    )
-    cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO weather_data (timestamp, city, temperature, wind_speed, temp_max, temp_min) "
-        "VALUES (%s, %s, %s, %s, %s, %s)",
-        (timestamp, city, temperature, wind_speed, temp_max, temp_min)
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        # Verbindung zur Datenbank herstellen
+        conn = pg8000.connect(
+            user='postgres',
+            password='Monster',
+            host='localhost',
+            port=5432
+        )
+
+        # Cursor erstellen
+        cur = conn.cursor()
+
+        # Daten in die Tabelle einfügen
+        cur.execute(
+            "INSERT INTO weather_data (timestamp, city, temperature, wind_speed, temp_max, temp_min) "
+            "VALUES (%s, %s, %s, %s, %s, %s)",
+            (timestamp, city, temperature, wind_speed, temp_max, temp_min)
+        )
+
+        # Änderungen bestätigen und Cursor schließen
+        conn.commit()
+        cur.close()
+
+    except pg8000.Error as e:
+        print('Fehler beim Speichern der Daten in die Datenbank:', e)
 
 
 if __name__ == "__main__":
