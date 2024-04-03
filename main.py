88afa5781c8f2b140032af69193851f8fb303146
JuliaThom <juliathom89@web.de>
2024-04-03 23:05:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+from datetime import datetime
+import psycopg2
+
+API_KEY = '7b256a5921981a44c938b40908d30411'  # Bitte fügen Sie hier Ihren API-Schlüssel ein
+CITY = 'Berlin'  # Beispielstadt
+
+def fetch_weather_data():
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    if 'main' in data and 'wind' in data:
+        city = data['name']
+        temperature = data['main']['temp']
+        wind_speed = data['wind']['speed']
+        temp_max = data['main']['temp_max']
+        temp_min = data['main']['temp_min']
+        timestamp = datetime.now()
+        save_to_database(timestamp, city, temperature, wind_speed, temp_max, temp_min)
+    else:
+        print("Fehler: Keine Daten gefunden.")
+
+def save_to_database(timestamp, city, temperature, wind_speed, temp_max, temp_min):
+    conn = psycopg2.connect(
+        dbname='weather',
+        user='postgres',
+        password='Miau',
+        host='localhost',
+        port='5432'
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO weather_data (timestamp, city, temperature, wind_speed, temp_max, temp_min) "
+        "VALUES (%s, %s, %s, %s, %s, %s)",
+        (timestamp, city, temperature, wind_speed, temp_max, temp_min)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    fetch_weather_data()
